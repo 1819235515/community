fa5a0cdcd3e6171de03223ade58c351357530b8d
1819235515 <66718288+1819235515@users.noreply.github.com>
2021-06-02 01:46:30 +0800

    init repo

---
+++ src/main/java/com/community/jxnu/jxnu/JxnuApplication.java
@@ -0,0 +1,13 @@
+package com.community.jxnu.jxnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxnuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JxnuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/community/jxnu/jxnu/JxnuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.community.jxnu.jxnu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JxnuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
